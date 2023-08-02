e526a306af7567663b3e9f223a531271a3327ded
vitot <victorfarian@gmail.com>
2023-08-01 23:01:46 -0300

    Modificando o projeto para intruduzir o observer

---
+++ src/br/com/victotExe/cm/modelo/Campo.java
@@ -0,0 +1,127 @@
+package br.com.victotExe.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {// um quadradinho do campo
+
+	private final int linha;
+	private final int coluna;
+
+	private boolean aberto = false;// se abriu ou n�o
+	private boolean minado = false;// se tem bomba ou n�o
+	private boolean marcado = false;// se est� marcado ou n�o
+
+	private List<Campo> vizinhos = new ArrayList<>();
+
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	boolean adicionarVizinho(Campo vizinho) {
+		boolean linhaDiferente = linha != vizinho.linha;
+		boolean colunaDiferente = coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+
+		int deltaLinha = Math.abs(linha - vizinho.linha);
+		int deltaColuna = Math.abs(coluna - vizinho.coluna);
+		int deltaGeral = deltaLinha + deltaColuna;
+
+		if (deltaGeral == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else if (deltaGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	void alternarMarcacao() {
+		if(!aberto) {
+			marcado = !marcado;
+		}
+	}
+	
+	boolean abrir() {
+		
+		if(!aberto && !marcado) {
+			aberto = true;
+			
+			if(minado) {
+				//TODO implementar nova versão (TODO é para algo que ainda vai fazer
+				//FIXME (algum erro que vai concertar
+			}
+			
+			if(vizinhancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());//passa um consumer que vai abrir os vizinhos de forma recursiva, at� encontrar um vizinho que n�o possa ser aberto
+				
+			}
+			
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	boolean vizinhancaSegura() {
+		//Predicate<Campo> seguro = a -> a.minado == false;//minha tentativa
+		return vizinhos.stream().noneMatch(v -> v.minado);//se nenhum for minado ele retorna true
+				//.anyMatch(seguro);//minha tentativa
+	}
+	
+	void minar() {
+		this.minado = true;
+	}
+	
+	public boolean isMinado() {
+		return this.minado;
+	}
+	
+	public boolean isMarcado() {//m�todo getter de atributos boolean
+		return this.marcado;
+	}
+	
+//	public void setMinado(boolean b) {//minha altera��o para mudar o valor de campo.minado
+//		this.minado = b;
+//	}
+	
+	void setAberto(boolean aberto) {
+		this.aberto = aberto;
+	}
+	
+	public boolean isAberto() {
+		return this.aberto;
+	}
+	
+	public boolean isFechado() {
+		return !this.isAberto();//fechado � o contrario do aberto
+	}
+//testar
+	public int getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+	
+	boolean objetivoAlcancado() {
+		boolean desvendado = !minado && aberto;
+		boolean protegido = minado && marcado;
+		return desvendado || protegido;
+	}
+	
+	long minasNaVizinhanca() {
+		return vizinhos.stream().filter(v -> v.minado).count();//conta quantos est�o minados e retorna
+	}
+	
+	void reiniciar() {
+		aberto = false;
+		minado = false;
+		marcado = false;
+	}
+//toString removido	
+}
---
+++ src/br/com/victotExe/cm/modelo/Tabuleiro.java
@@ -0,0 +1,86 @@
+package br.com.victotExe.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Tabuleiro {
+
+	private int linhas;
+	private int colunas;
+	private int minas;
+
+	private final List<Campo> campos = new ArrayList<>();// foi utilizada uma List ao inv�s de matriz para podermos
+															// utilizar o API de Streams
+	// quanto mais restrito for a visibilidade melhor fica, pois o c�digo fica mais
+	// independente tornando a manuten��o mais facil
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		// ap�s o tabuleiro ser iniciado ele vai executar alguns m�todos
+		// automaticamente.
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+	
+	public void abrir(int linha, int coluna) {
+		
+		try {
+			campos.parallelStream()
+			.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+			.findFirst()
+			.ifPresent(c -> c.abrir());
+		}catch (Exception e) {
+			//FIXME Ajustar a implementação do método abrir
+			campos.forEach(c -> c.setAberto(true));
+			throw e;
+		}
+	}		
+	
+	public void alterarMarcacao(int linha, int coluna) {
+		campos.parallelStream()
+		.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+		.findFirst()
+		.ifPresent(c -> c.alternarMarcacao());
+	}
+	
+	private void gerarCampos() {//m�todo que vai gerar os campos
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int coluna = 0; coluna < colunas; coluna++) {
+				campos.add(new Campo(linha, coluna));
+			}
+		}
+	}
+	
+	private void associarVizinhos() {
+		for(Campo c1: campos) {//for que percorre todos os campos e associa os vizinhos
+			for(Campo c2: campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+	
+	private void sortearMinas() {
+		long minasArmadas= 0;
+		Predicate<Campo> minado = c -> c.isMinado();
+		do {
+			int aleatorio =(int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();//minar antes de contar se n�o o jogo fica com uma mina a mais
+//			minasArmadas = (int)campos.stream().filter(minado).count();//jeito para fazer o cast(convers�o) de long para int
+			minasArmadas = campos.stream().filter(minado).count();//aqui apenas mudamos o tipo do atributo na constru��o
+		}while(minasArmadas < minas);
+	}
+	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+	
+}
