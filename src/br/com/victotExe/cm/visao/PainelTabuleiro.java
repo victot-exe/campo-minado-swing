c8b1e0d5bd28cd43ef7de4470d2a75ebbd2c3f4a
vitot <victorfarian@gmail.com>
2023-08-03 21:50:24 -0300

    Começo da interface gráfica(criando a janela)

---
+++ src/br/com/victotExe/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,12 @@
+package br.com.victotExe.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.victotExe.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {// JPanel é um container(componente que agrupa outros componentes)
+	
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+	}
+}
---
+++ src/br/com/victotExe/cm/visao/TelaPrincipal.java
@@ -0,0 +1,31 @@
+package br.com.victotExe.cm.visao;
+
+import javax.swing.JFrame;
+
+import br.com.victotExe.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class TelaPrincipal extends JFrame{
+	
+	public TelaPrincipal() {
+//		definindo o tabuleiro
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+//		adicionando o tabuleiro na janela
+		add(new PainelTabuleiro(tabuleiro));
+		
+		
+		
+		setTitle("Campo Minado");
+//		definirOTamanho(largura, altura)
+		setSize(690, 438);
+//		posição da janela(null = centralizar)
+		setLocationRelativeTo(null);
+//		botão X fecha
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new TelaPrincipal();
+	}
+}
